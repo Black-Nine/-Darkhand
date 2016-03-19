ebe080bd3520ea43749aebb7a6f63182ee6a771f
TELE BN <neorabbit.m@gmail.com>
2016-03-19 10:26:08 +0330

    t

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -123,7 +123,7 @@ end
 
 -- Go over enabled plugins patterns.
 function match_plugins(msg)
-  for name, plugin in pairs(plugins) do
+  for name, plugins in pairs(plugins) do
     match_plugin(plugin, name, msg)
   end
 end
